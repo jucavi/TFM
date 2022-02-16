b33ebc8075fe5f35cf1351da73c09e4c4666f37e
Jucavi <jcarvil@gmail.com>
2022-02-16 18:42:17 +0100

    setup sqlalqchemy

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-def create_app(environment='development'):
+db = SQLAlchemy()
+
+
+def create_app(environment='develop'):
     from config import config
+
 
     app = Flask(__name__)
     app.config.from_object(config.get(environment))
+
+    db.init_app(app)
 
     @app.route('/welcome')
     def welcome():
