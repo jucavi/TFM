a02e201fceceb0992f440eb8893266f34d6549f1
Jucavi <jcarvil@gmail.com>
2022-02-18 17:07:31 +0100

    add routes

--- project/auth/auth.py
+++ project/auth/auth.py
@@ -3,3 +3,22 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__)
 
+
+@auth_bp.route('/signup')
+def signup():
+    return 'Signup'
+
+
+@auth_bp.route('/login')
+def login():
+    return 'Login'
+
+
+@auth_bp.route('/logout')
+def logout():
+    return 'Logout'
+
+
+@auth_bp.route('/profile')
+def profile():
+    return 'profile'
