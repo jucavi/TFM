4058f2c2edbe79241d00ed50b660742447993908
jucavi <jcarvil@gmail.com>
2022-02-18 13:22:39 +0100

    add flash messages

--- project/home/home.py
+++ project/home/home.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
 home_bp = Blueprint('home', __name__, static_folder='static', template_folder='templates')
 
 
 @home_bp.route('/')
 def home():
+    flash('This is a error', category='danger')
     return render_template('home.html')
