693aeaec42cf33dfa2fd0702b687a5368c95c810
Jucavi <jcarvil@gmail.com>
2022-02-21 09:25:28 +0100

    add workspace route

--- project/home/home.py
+++ project/home/home.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template
+from flask_login import login_required
+
 
 home_bp = Blueprint('home', __name__, static_folder='static', template_folder='templates')
 
 
 @home_bp.route('/')
 def home():
-    flash('This is a error', category='danger')
+    return render_template('home.html')
+
+
+@home_bp.route('/workspace')
+@login_required
+def workspace():
+    return render_template('workspace.html')
+
+
+@home_bp.route('/<_>')
+def missing_route(_):
     return render_template('home.html')
