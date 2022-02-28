512b2b7ed49e3b87c2ed88a10cbe4c5fb3b1207a
Jucavi <jcarvil@gmail.com>
2022-02-28 17:28:18 +0100

    add user_projects joining table

--- app/project/project_model.py
+++ app/project/project_model.py
@@ -3,6 +3,13 @@ from sqlalchemy_utils import UUIDType
 import uuid
 from datetime import datetime
 
+
+user_projects = db.Table('user_projects',
+    db.Column('user_id', UUIDType(binary=False), db.ForeignKey('user.id'), nullable=False),
+    db.Column('project_id', UUIDType(binary=False), db.ForeignKey('project.id'), nullable=False),
+    db.Column('is_owner', db.Boolean, default=False)
+)
+
 class Project(db.Model):
 
     __tablename__ = 'project'
@@ -29,10 +36,12 @@ class Project(db.Model):
         db.DateTime,
         nullable = False
     )
+    users = db.relationship('User', secondary=user_projects, lazy='subquery', backref=db.backref('projects', lazy=True))
 
     def __init__(self, projectname):
         self.projectname = projectname
         self.created_at = self.updated_at = datetime.now()
+
 
     def __repr__(self):
         return f'<Project: {self.projectname!r} created_at: {self.created_at}>'
