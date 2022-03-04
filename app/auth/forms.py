8dd501c11d659beeeeaba0d6c228f8f5b6eb3020
Jucavi <jcarvil@gmail.com>
2022-03-04 14:03:16 +0100

    refactor forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,11 +4,14 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Regexp, ValidationError
 
 
-class SignUpForm(FlaskForm):
+class UserDataForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email(message='Invalid email.')])
+
+
+class PasswordForm(FlaskForm):
     password = PasswordField(
         'Password',
         validators=[
@@ -30,6 +33,9 @@ class SignUpForm(FlaskForm):
             EqualTo('password', message='Passwords must match.')
         ]
     )
+
+
+class SignUpForm(UserDataForm, PasswordForm):
     submit = SubmitField('Sign Up')
 
 
@@ -39,22 +45,14 @@ class LogInForm(FlaskForm):
     submit = SubmitField('Log In')
 
 
-class EditProfileForm(FlaskForm):
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email(message='Invalid email.')])
-
+class EditProfileForm(UserDataForm):
     # TODO about, preferences
-
     submit = SubmitField('Edit Profile')
-
 
     def __init__(self, current_username, current_email, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
         self.current_username = current_username
         self.current_email = current_email
-
 
     def validate_username(self, username):
         if username.data != self.current_username:
@@ -62,7 +60,6 @@ class EditProfileForm(FlaskForm):
 
             if user is not None:
                 raise ValidationError('Username already in use.')
-
 
     def validate_email(self, email):
         if email.data != self.current_email:
@@ -76,28 +73,6 @@ class RequestNewPasswordForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(), Email(message='Invalid email.')])
     submit = SubmitField('Send')
 
-#TODO refactor
 
-class SetNewPasswordForm(FlaskForm):
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            Regexp(
-                '^(?=\S{6,}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])',
-                message='The password must contain at least: ' \
-                        'Uppercase characters, ' \
-                        'lowercase characters, ' \
-                        'digits, ' \
-                        'special characters.'
-                )
-        ]
-    )
-    confirm = PasswordField(
-        'Confirm',
-        validators=[
-            DataRequired(),
-            EqualTo('password', message='Passwords must match.')
-        ]
-    )
+class SetNewPasswordForm(PasswordForm):
     submit = SubmitField('Set Password')
--- app/project/forms.py
+++ app/project/forms.py
@@ -2,9 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
-
-class NewProjectForm(FlaskForm):
+class ProjectBasicForm(FlaskForm):
     project_name = StringField('Project name', validators=[DataRequired()])
     project_desc = TextAreaField('Project description', validators=[DataRequired()])
 
-    submit = SubmitField('New Project')
+    submit = SubmitField()
+
+
+class NewProjectForm(ProjectBasicForm):
+    submit = SubmitField('Create Project')
+
+
+class EditProjectForm(ProjectBasicForm):
+    submit = SubmitField('Edit Project')
