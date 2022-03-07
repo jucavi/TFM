c2eef4ec56d6a0d3e126924be6b9685825622775
Jucavi <jcarvil@gmail.com>
2022-03-07 15:29:56 +0100

    add mail invitation for collab

--- app/helpers/mail.py
+++ app/helpers/mail.py
@@ -34,6 +34,18 @@ def send_password_reset_email(user):
         subject='TFT Password Reset',
         sender=('TFT', current_app.config['MAIL_DEFAULT_SENDER']),
         recipients=[user.email],
-        text_body=render_template('mailer/reset_password.html', token=token, user=user),
-        html_body=render_template('mailer/reset_password.html', token=token, user=user)
+        text_body=render_template('mailer/auth/reset_password.txt', token=token, user=user),
+        html_body=render_template('mailer/auth/reset_password.html', token=token, user=user)
+    )
+
+
+def send_project_invitation(project, user):
+    token = project.collaborator_token(user)
+
+    send_email(
+        subject='Project join invitation',
+        sender=('TFT', current_app.config['MAIL_DEFAULT_SENDER']),
+        recipients=[user.email],
+        text_body=render_template('mailer/project/project_invitation.txt', token=token, user=user, project=project),
+        html_body=render_template('mailer/project/project_invitation.html', token=token, user=user, project=project)
     )
