6b0c6c59e71d0733fc96153854c208e1bcc56c43
Jucavi <jcarvil@gmail.com>
2022-03-13 12:51:07 +0100

    add property received and created

--- app/message/models.py
+++ app/message/models.py
@@ -1,9 +1,9 @@
 from app import db
 from sqlalchemy_utils import UUIDType
-from sqlalchemy import Column,DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, DateTime, ForeignKey, Boolean, String
 import uuid
 from datetime import datetime
-
+from app.helpers.date import local_time
 
 class Message(db.Model):
     __tablename__ = 'message'
@@ -16,9 +16,22 @@ class Message(db.Model):
     )
     sender_id = Column(UUIDType(binary=False), ForeignKey('user.id'))
     recipient_id = Column(UUIDType(binary=False), ForeignKey('user.id'))
-    body = Column(db.String(140))
+    body = Column(String())
     timestamp = Column(DateTime, index=True, default=datetime.utcnow)
     read = Column(Boolean(), default=False)
+
+    @property
+    def received(self):
+        tn = datetime.utcnow()
+        lt = local_time(self.timestamp)
+        if (tn.day - self.timestamp.day) > 0:
+            return lt.strftime('%d %b')
+        return lt.strftime('%H:%M')
+
+    @property
+    def created(self):
+        return local_time(self.timestamp)
+
 
     def __repr__(self):
         return f'<Message {self.body}>'
