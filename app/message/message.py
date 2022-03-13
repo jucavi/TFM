31a3a133583be82b0cfeaaf415bdaf8cfc5c40a1
Jucavi <jcarvil@gmail.com>
2022-03-13 12:54:09 +0100

    modify all to messages route

--- app/message/message.py
+++ app/message/message.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 from app.message.forms import MessageForm
@@ -20,21 +21,24 @@ def send_message():
     if form.validate_on_submit():
         recipient = User.query.filter_by(email=form.email.data).first()
         if recipient:
-            message = Message(sender_id=current_user.id, recipient_id=recipient.id, body=form.message.data)
+            message = Message(author=current_user, recipient=recipient, body=form.message.data)
 
             db.session.add(message)
             db.session.commit()
             flash('Your message has been sent.', category='success')
             return redirect(url_for('project.all'))
         else:
-            flash('')
+            flash('User not found.', category='warning')
     return render_template('send_message.html',
                            title='Send message',
                            form=form)
 
 
-@msg_bp.route('/all')
+@msg_bp.route('/messages')
 @login_required
-def all():
-    messages = Message.query.all()
-    return 'All messages'
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+
+    messages = current_user.messages_received.order_by(Message.timestamp.desc())
+    return render_template('messages.html', messages=messages)
