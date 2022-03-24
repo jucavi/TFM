648a84c25c5550ed204e47dbadf56ff8d8604716
Jucavi <jcarvil@gmail.com>
2022-03-24 13:44:21 +0100

    refactor addTreeView

--- app/project/static/js/file_handler.js
+++ app/project/static/js/file_handler.js
@@ -1,46 +1,18 @@
 const tree = document.querySelector('#tree');
+const baseURL = document.URL;
 
 window.onload = function () {
-  const root = JSON.parse(tree.querySelector('#tree_root').value);
-  addTreeView([{ id: root.id, name: root.name }], [], tree);
+  const root = tree.querySelector('li');
+  root.addEventListener('click', expandCollapseToggler);
 };
-
-function addTreeView(children, files, folderContent) {
-  for (let child of children) {
-    const divWrapper = document.createElement('div');
-    divWrapper.classList.add('folder_wrapper');
-    divWrapper.setAttribute('id', child.id);
-
-    const li = document.createElement('li');
-    li.classList.add('folder');
-    li.innerText = child.name;
-    li.addEventListener('click', expandCollapseToggler);
-    divWrapper.append(li);
-
-
-    const ul = document.createElement('ul');
-    ul.classList.add('folder_content');
-    ul.classList.add('fa-ul');
-    divWrapper.append(ul);
-
-    folderContent.append(divWrapper);
-  }
-
-  for (let file of files) {
-    const li = document.createElement('li');
-    li.setAttribute('id', file.id);
-    li.innerText = file.name;
-    folderContent.append(li);
-  }
-}
 
 async function expandCollapseToggler(event) {
   event.stopPropagation();
   const id = event.target.parentNode.id;
-  const content = event.target.parentNode.lastChild
+  const content = event.target.parentNode.lastElementChild;
 
   if (content.innerHTML === '') {
-    const url = `${document.URL}/data/${id}`;
+    const url = `${baseURL}/data/${id}`;
     try {
       const { data } = await axios.get(url);
       addTreeView(data.children, data.files, content);
@@ -50,4 +22,57 @@ async function expandCollapseToggler(event) {
   } else {
     content.toggleAttribute('hidden');
   }
+}
+
+function createLink(name, url) {
+  const a = document.createElement('a');
+  a.innerText = name;
+  a.href = url;
+  a.classList.add('btn');
+  a.classList.add('btn-info');
+
+  return a;
+}
+
+function addFolders(children, folderContent) {
+  for (let child of children) {
+    const divWrapper = document.createElement('div');
+    divWrapper.classList.add('folder_wrapper');
+    divWrapper.setAttribute('id', child.id);
+
+    const url = `${baseURL}/folder/${child.id}?name=${child.name}-newFolderJS`
+    const renameLink = createLink('Rename', '#');
+    const deleteLink = createLink('Delete', '#');
+    const addFolderLink = createLink('Add Folder', url);
+
+    const li = document.createElement('li');
+    li.classList.add('folder');
+    li.innerText = child.name;
+    li.addEventListener('click', expandCollapseToggler);
+    divWrapper.append(li);
+    divWrapper.append(addFolderLink);
+    divWrapper.append(renameLink);
+    divWrapper.append(deleteLink);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('folder_content');
+    ul.classList.add('fa-ul');
+    divWrapper.append(ul);
+
+    folderContent.append(divWrapper);
+  }
+}
+
+function addFiles(files, folderContent) {
+  for (let file of files) {
+    const li = document.createElement('li');
+    li.setAttribute('id', file.id);
+    li.innerText = file.name;
+    folderContent.append(li);
+  }
+}
+
+function addTreeView(children, files, folderContent) {
+  addFolders(children, folderContent);
+  addFiles(files, folderContent);
 }
